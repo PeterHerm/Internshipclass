e32ed6c5f58cd7a87b96e38db515611509df9f01
PeterHerm <herman.peter33@yahoo.com>
2021-03-25 11:25:14 +0200

    StartupGresit

--- Startup.cs
+++ Startup.cs
@@ -48,7 +48,7 @@ namespace RazorMvc
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
+                    
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
