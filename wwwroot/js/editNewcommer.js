739538972357ad6867df11bb93811ed1512025e9
PeterHerm <herman.peter33@yahoo.com>
2021-04-01 10:22:06 +0300

    client side update handler

---
+++ wwwroot/js/editNewcommer.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+
+    $("#list").on("click", ".startEdit", function () {
+        var targetMemberTag = $(this).closest('li');
+        var index = targetMemberTag.index();
+        var currentName = targetMemberTag.find(".name").text();
+        $('#editClassmate').attr("memberIndex", index);
+        $('#classmateName').val(currentName);
+    })
+
+    $("#editClassmate").on("click", "#submit", function () {
+        console.log('submit changes to server');
+
+        var newName = $('#classmateName').val();
+        var index = $('#editClassmate').attr("memberIndex");
+
+        console.log(`/Home/UpdateMember?index=${index}&name=${newName}`);
+
+        $.ajax({
+            method: "PUT",
+            url: `/Home/UpdateMember?index=${index}&name=${newName}`,
+            success: function (data) {
+                $('.name').eq(index).replaceWith(newName);
+            },
+            error: function (data) {
+                alert(`Failed to update`);
+            },
+        });
+    })
+
+    $("#editClassmate").on("click", "#cancel", function () {
+        console.log('cancel changes');
+    })
+
+});
