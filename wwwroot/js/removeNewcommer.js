d7d52a9976e870905f7a9703ca2dfcfa5b089b4b
PeterHerm <herman.peter33@yahoo.com>
2021-04-01 10:24:47 +0300

    add modal box on modify image

---
+++ wwwroot/js/removeNewcommer.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    $("#list").on("click", ".remove", function () {
+
+        var $li = $(this).closest('li');
+        var index = $li.index();
+
+        console.log(`index=${index}`);
+        console.log(`$li=${$li}`);
+
+        $.ajax({
+            method: "DELETE",
+            url: `/Home/RemoveMember?index=${index}`,
+            success: function (data) {
+
+                $li.remove();
+
+            },
+            error: function (data) {
+                alert(`Failed to remove`);
+            },
+        });
+    })
+
+});
