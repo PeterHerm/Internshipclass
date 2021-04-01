9ae1e99fb04c45fe1eb465cf8d16cbc9400644d0
PeterHerm <herman.peter33@yahoo.com>
2021-04-01 10:23:46 +0300

    add setinterval for populateForecast

---
+++ wwwroot/js/populateForecast.js
@@ -0,0 +1,38 @@
+﻿function refreshWeatherForecast() {
+    $(document).ready(function () {
+
+        $.ajax({
+            url: "/WeatherForecast",
+            success: function (result) {
+                console.log(result);
+                let tomorrow = result[0];
+                let tomorrowDate = formatDate(tomorrow.date);
+
+                $("#date").text(tomorrowDate);
+                $("#temperature").text(tomorrow.temperatureC, ' C');
+                $("#summary").text(tomorrow.summary);
+            },
+            error: function () {
+                console.log('Failed to get data');
+            }
+        });
+    });
+}
+
+    setInterval(refreshWeatherForecast, 3000);
+
+    function formatDate(jsonDate) {
+
+        function join(t, a, s) {
+            function format(m) {
+                let f = new Intl.DateTimeFormat('en', m);
+                return f.format(t);
+            }
+            return a.map(format).join(s);
+        }
+
+        let date = new Date(jsonDate);
+        let a = [{ day: 'numeric' }, { month: 'short' }, { year: 'numeric' }];
+        let s = join(date, a, '-');
+        return s;
+    }
