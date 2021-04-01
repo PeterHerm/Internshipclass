338d6bfc3600c0e102dc13567df7a64ed6a6209c
PeterHerm <herman.peter33@yahoo.com>
2021-04-01 10:04:37 +0300

    shortened code

---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace RazorMvc.WebAPI
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC => (int)(TemperatureK - 273.15);
+
+        public string Summary { get; set; }
+
+        public double TemperatureK{ get; set; }
+    }
+}
