96bc9851a1909d6ea2eee666f93797456b5d5e97
PeterHerm <herman.peter33@yahoo.com>
2021-04-01 10:20:09 +0300

    modified button handler

---
+++ wwwroot/js/addNewcommers.js
@@ -0,0 +1,27 @@
+﻿// This JS file now uses jQuery. Pls see here: https://jquery.com/
+$(document).ready(function () {
+    // see https://api.jquery.com/click/
+    $("#add").click(function () {
+        var newcomerName = $("#newcomer").val();
+
+        // Remember string interpolation
+        $.ajax({
+            url: `/Home/AddMember?member=${newcomerName}`,
+            success: function (data) {
+                // Remember string interpolation
+                $("#list").append(`<li class="member"><span class="name">${data}</span><span class="remove fa fa-remove"></span><i class="startEdit fa fa-pencil" data-toggle="modal" data-target="#editClassmate"></i>
+		        </li>`);
+
+                $("#newcomer").val("");
+            },
+            error: function (data) {
+                alert(`Failed to add ${newcomerName}`);
+            },
+        });
+
+    })
+
+    $("#clear").click(function () {
+        $("#newcomer").val("");
+    })
+});
