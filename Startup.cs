12f14567000154cb3d35adec4be21e60c67d321c
PeterHerm <herman.peter33@yahoo.com>
2021-04-01 10:13:37 +0300

    add documentation to WeatherForecastController

--- Startup.cs
+++ Startup.cs
@@ -3,7 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using RazorMvc.Services;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace RazorMvc
 {
@@ -21,6 +25,15 @@ namespace RazorMvc
         {
             services.AddControllersWithViews();
             services.AddSingleton(typeof(InternshipService));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RazorMvc.WebAPI", Version = "v1" });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +42,8 @@ namespace RazorMvc
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RazorMvc.WebAPI v1"));
             }
             else
             {
